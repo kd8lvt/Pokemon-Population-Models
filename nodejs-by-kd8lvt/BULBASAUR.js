66a6ca23022ec598b1e81d48471be48d3c4475ca
kd8lvt <redunderpant@gmail.com>
2019-07-24 21:23:24 -0400

    Make graph easier to use. General code cleanup.

--- nodejs-by-kd8lvt/BULBASAUR.js
+++ nodejs-by-kd8lvt/BULBASAUR.js
@@ -1,32 +1,15 @@
-const Population = require('./Population.js');
+const Test = require('./Test.js');
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const thisCsv = createCsvWriter({
-	path:'./web/bulbasaur.csv',
-	header: [
-		{id:'tick',title:'tick'},
-		{id:'females',title:'females'},
-		{id:'males',title:'males'}
-	]
-});
-
-let interval = null;
-async function start() {
-	let pop = new Population({fertility:{humanFertRate:10,humanInterference:true}});
-	let ticks = 0;
-	interval = setInterval(()=>{
-		pop.tick();
-		if (pop.totalPop >= 20000) {
-			console.log('Stopped in '+ticks+' ticks due to "max population reached"');
-			thisCsv.writeRecords(pop.dataArr);
-			clearInterval(interval);
-		} else if (pop.extinct) {
-			console.log('Stopped in '+ticks+' ticks due to "extinct"');
-			thisCsv.writeRecords(pop.dataArr);
-			clearInterval(interval);
+if (process.argv[2] && process.argv[2].toLowerCase() == 'humans') {
+	let test = new Test({
+		population: {
+			sexRates: {outOf: 8, mRate: 7},
+			fertility: {humanFertRate: 10, humanInterference: true}
 		}
-		ticks++
-	},50);
+	});
+	test.start();
+} else (process.argv[2] == null)
+{
+	let test = new Test();
+	test.start();
 }
-
-start();
--- nodejs-by-kd8lvt/web/graphvisualizer.js
+++ nodejs-by-kd8lvt/web/graphvisualizer.js
@@ -3,91 +3,101 @@
 //****I WILL BE DOING VERY LITTLE MAINTENANCE****
 
 
-let table;
-let input;
-let button;
-let scalingB;
 let scaleGraphs = true;
-
+let table,input,button,scalingB,div,canvas,minPop,maxPop,totalPops,malPops,femPops;
+let arrays = [totalPops,malPops,femPops];
 function preload() {
     input = createInput('http://localhost:8080/bulbasaur.csv');
     button = createButton('Load CSV');
     button.mouseClicked(loadCSV);
-    scalingB = createButton('Scale? '+scaleGraphs.toString());
+    scalingB = createButton('Scale? ' + scaleGraphs.toString());
     scalingB.mouseClicked(scaleGraph);
+    div = createDiv(); //Keep the inputs and output on separate lines
+    totalPops=[];malPops=[];femPops = [];
     loadCSV();
 }
-
+function setup() {
+    canvas = createCanvas(500,500);
+    //canvas.mouseMoved(checkX);
+}
 function scaleGraph() {
     scaleGraphs = !scaleGraphs;
     this.html("Scale? "+scaleGraphs.toString());
+
 }
 
 function draw() {
-    createDiv(); //Keep the inputs and output on separate lines
-    createCanvas(500,500);
-    background(0);
-    stroke(255);
-    let totalPops = [];
-    let femPops = [];
-    let malPops = [];
-
     let arr = table.getArray();
 
     for (let i=0;i<arr.length;i++) {
-        totalPops[i] = arr[i][1] + arr[i][2]; //Convert to total pops.
+        totalPops[i] = parseInt(parseInt(arr[i][1]) + parseInt(arr[i][2])); //Convert to total pops.
         femPops[i] = arr[i][1]; //Add female
         malPops[i] = arr[i][2]; //And male
     }
 
-    let minPop = min(totalPops);
-    let maxPop = max(totalPops);
-    let minPopF = min(femPops);
-    let maxPopF = max(femPops);
-    let minPopM = min(malPops);
-    let maxPopM = max(malPops);
+    minPop = min(totalPops);
+    maxPop = max(totalPops);
 
-    let lastX,lastY,lastXF,lastYF,lastXM,lastYM = 0;
-    for (let i = 0; i < totalPops.length; i++) {
-        let val = totalPops[i];
+    background(0);
+    strokeWeight(1);
+    stroke(255);
 
-        let xpos = map(i, 0, totalPops.length, 0, width);
-        let ypos = map(val, minPop, maxPop, height, 0);
+    drawGraph({r:255,g:255,b:255},totalPops);
+    drawGraph({r:255,g:105,b:180},femPops);
+    drawGraph({r:0,g:191,b:255},malPops);
+}
+
+function drawGraph(rgb,arr) {
+    let lastX,lastY=0;
+    stroke(rgb.r,rgb.g,rgb.b,255);
+    minPopNS = min(arr);
+    maxPopNS = max(arr);
+    for (let i = 0; i < arr.length; i++) {
+        let val = arr[i];
+        xpos = i;//map(i, 0, arr.length, 0, width);
+        if (scaleGraphs) {
+            ypos = map(val, minPop, maxPop, height-50, 0)+50;
+        } else {
+            ypos = map(val, minPopNS, maxPopNS, height-50, 0)+50;
+        }
 
         line(xpos,ypos,lastX,lastY);
+
         lastX = xpos;
         lastY = ypos;
-    }
-    stroke(255,105,180);
-    for (let i = 0; i < femPops.length; i++) {
-        let val = femPops[i];
-        let xpos,ypos = 0;
-        if (scaleGraphs) {
-            xpos = map(i, 0, femPops.length, 0, width);
-            ypos = map(val, minPop, maxPop, height, 0);
-        } else {
-            xpos = map(i, 0, femPops.length, 0, width);
-            ypos = map(val, minPopF, maxPopF, height, 0);
-        }
-        line(xpos,ypos,lastXF,lastYF);
-        lastXF = xpos;
-        lastYF = ypos;
-    }
-    stroke(0,191,255);
-    for (let i = 0; i < malPops.length; i++) {
-        let val = malPops[i];
-        let xpos,ypos = 0;
-        if (scaleGraphs) {
-            xpos = map(i, 0, malPops.length, 0, width);
-            ypos = map(val, minPop, maxPop, height, 0);
-        } else {
-            xpos = map(i, 0, malPops.length, 0, width);
-            ypos = map(val, minPopM, maxPopM, height, 0);
-        }
 
-        line(xpos,ypos,lastXM,lastYM);
-        lastXM = xpos;
-        lastYM = ypos;
+        if (mouseX <= width && mouseX >= 0) {
+            if (xpos == mouseX) {
+                stroke(255,255,0);
+                line(mouseX,0,mouseX,height);
+                noStroke();
+                fill(rgb.r,rgb.g,rgb.b)
+                text(val, xpos, ypos - 30);
+                stroke(rgb.r,rgb.g,rgb.b)
+            }
+        }
+    }
+}
+
+function checkX() {
+    if (mouseX <= 500 && mouseX >= 0) {
+        for (let j = 0; j < arrays; j++) {
+            let arr = arrays[j];
+            for (let k = 0; k < arr.length; k++) {
+                let xpos = k;//map(k, 0, arr.length, 0, width);
+                if (scaleGraphs) {
+                    ypos = map(val, minPop, maxPop, height, 0);
+                } else {
+                    ypos = map(val, minPopNS, maxPopNS, height, 0);
+                }
+                if (k === mouseX) {
+                    strokeWeight(10);
+                    point(xpos, ypos);
+                    text(arr[i], xpos, ypos - 20);
+                    strokeWeight(1);
+                }
+            }
+        }
     }
 }
 
